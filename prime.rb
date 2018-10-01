a0b4d37a8ec696346ef95cafbefe1b0b3b2fc985
Jantastication O <georginaoregx@gmail.com>
2018-10-01 22:04:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(integer)
   if integer >= 2
-      (2..integers - 1).all? do |integer|
-        integers % integer != 0
+      (2..integer - 1).all? do |integer|
+        integer % integer != 0
       end
     else
       return false
